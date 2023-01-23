aca82357976818cdf79fac94d2992dfd9a59b266
Jonimarti <jonimarti@users.noreply.github.com>
2023-01-23 18:55:54 +0100

    initial upload

---
+++ Assets/Scripts/CreateCharacter/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace Player
+{
+	
+	public class Character
+	{
+		public string name;
+		public Character(string name)
+		{
+			this.name = name;
+			Debug.Log("Hola, soy" + name);
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/CreateCharacter/CreateCharacter.cs
@@ -0,0 +1,22 @@
+﻿using Player;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class CreateCharacter : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+			new Character(this.name);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
