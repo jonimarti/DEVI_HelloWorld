f09f2aab2cdc2fa28597812b4290dca0c0e23987
Jonimarti <jonimarti@users.noreply.github.com>
2023-01-30 15:37:19 +0100

    Color The World

---
+++ Assets/Scripts/Gameobjects101/ColorTheWorld.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ColorTheWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Object[] allTheGameObjects;
+    [Serialize]
+    public Color color;
+    [Serialize]
+    public bool findByTag;
+    [Serialize]
+    public string choosenTag;
+    void Start()
+    {
+		if (findByTag)
+        {
+            allTheGameObjects = GameObject.FindGameObjectsWithTag(choosenTag);
+        }                             
+		else
+        {
+            allTheGameObjects = GameObject.FindObjectsOfType(typeof(MonoBehaviour));
+
+        }
+    }
+  
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+		foreach (var item in allTheGameObjects)
+		{
+        if(item.GetComponent<MeshRenderer>())
+         item.GetComponent<MeshRenderer>().material.color = color;
+        }
+        
+
+    }
+}
